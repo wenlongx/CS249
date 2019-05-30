e3d219fe82962f7558ed399b792dfe5d19decef3
Wenlong Xiong <wenlongx@gmail.com>
2019-05-30 14:47:31 -0700

    dataloader for elmo/bert files

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.utils.data as data
 from torch.autograd import Variable
 from load_pre_trained_embeddings import load_dataset,load_dataset_from_file
 from torch.nn.functional import softmax
+import numpy as np
+import h5py
+
+import sys
+import csv
+import argparse
 
 
 # Hyper Parameters
@@ -23,67 +30,114 @@ class LogisticRegression(nn.Module):
         out = self.sigmoid(self.linear(x))
         return out
 
-print("Loading Data")
-train_dataset = load_dataset_from_file("../train.csv","glove_mean_avg.pt")
-weights = torch.Tensor([x[1]*9+1 for x in train_dataset])
-sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, batch_size)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           sampler=sampler)
-test_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=False)
-print("Creating Model")
-model = LogisticRegression(input_size, num_classes)
-# model.load_state_dict(torch.load(PATH))
+# Dataset wrapper for the HDF5 files
+class HDF5_Dataset(data.Dataset):
+    def __init__(self, filepath, target_filepath):
+        self.filepath = filepath
 
-# Loss and Optimizer
-# Softmax is internally computed.
-# Set parameters to be updated.
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+        with open(target_filepath) as csv_file:
+            y = []
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                y.append(int(row[0]))
+            y = torch.from_numpy(np.array(y).astype(int))
+            self.targets = y
+        
+        self.len = len(self.targets)
 
-print("Starting Training")
-# Training the Model
-for epoch in range(num_epochs):
-    for i, (sentences, labels) in enumerate(train_loader):
+    # Return (vector_embedding, target)
+    def __getitem__(self, index):
+        with h5py.File(self.filepath, "r") as h5py_file:
+            return (h5py_file.get(str(index)), self.targets[index])
+
+    def __len__(self):
+        return self.len
+
+    def __get_targets__(self):
+        return self.targets
+
+
+if __name__ == "__main__":
+
+    # use args to specify embedding file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store", dest="train_filepath", help="This is the training file, with each of the training examples embedded already")
+    parser.add_argument("--targets", action="store", dest="target_filepath", help="This is the path to the training file's targets")
+    args = parser.parse_args()
+
+
+    """
+    python logistic_regression.py --train=quora-insincere-questions-classification/train_average.hdf5 --targets=quora-insincere-questions-classification/train_targets.csv
+    """
+
+    print("Loading Data")
+    if args.train_filepath == "":
+        args.train_filepath == "../train.csv"
+        train_dataset = load_dataset_from_file("../train.csv", "glove_mean_avg.pt")
+        weights = torch.Tensor([x[1]*9+1 for x in train_dataset])
+    else:
+        train_dataset = HDF5_Dataset(args.train_filepath, args.target_filepath)
+        targets = train_dataset.__get_targets__()
+        weights = torch.Tensor(list(map(lambda x: 15 if x == 0 else 1, targets)))
+
+    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, batch_size)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               sampler=sampler)
+    test_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False)
+    print("Creating Model")
+    model = LogisticRegression(input_size, num_classes)
+    # model.load_state_dict(torch.load(PATH))
+
+    # Loss and Optimizer
+    # Softmax is internally computed.
+    # Set parameters to be updated.
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    print("Starting Training")
+    # Training the Model
+    for epoch in range(num_epochs):
+        for i, (sentences, labels) in enumerate(train_loader):
+            sentences = Variable(sentences)
+            labels = Variable(labels)
+            print(labels.sum())
+
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()
+            outputs = model(sentences)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if (i) % 129 == 0:
+                torch.save(model.state_dict(), PATH)
+                print ('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f'
+                       % (epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data.item()))
+
+
+    print("Testing Model")
+    # Test the Model
+    model.eval()
+    correct = 0
+    total = 0
+    pred_positives = 0.0
+    real_positives = 0.0
+    true_positives = 0.0
+    for sentences, labels in test_loader:
         sentences = Variable(sentences)
-        labels = Variable(labels)
-        print(labels.sum())
-
-        # Forward + Backward + Optimize
-        optimizer.zero_grad()
         outputs = model(sentences)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        if (i) % 129 == 0:
-            torch.save(model.state_dict(), PATH)
-            print ('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f'
-                   % (epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data.item()))
-
-
-print("Testing Model")
-# Test the Model
-model.eval()
-correct = 0
-total = 0
-pred_positives = 0.0
-real_positives = 0.0
-true_positives = 0.0
-for sentences, labels in test_loader:
-    sentences = Variable(sentences)
-    outputs = model(sentences)
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    real_positives += labels.sum()
-    pred_positives += predicted.sum()
-    true_positives += (labels * predicted).sum().item()
-    correct += (predicted == labels).sum()
-    # break
-precision = true_positives*1.0/pred_positives.item()
-recall = true_positives*1.0/real_positives.item()
-print(true_positives,pred_positives,real_positives)
-print("F1 score: {}".format(2*(precision*recall)/(precision+recall)))
-print('Accuracy of the model: %d %%' % (100 * correct / total))
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        real_positives += labels.sum()
+        pred_positives += predicted.sum()
+        true_positives += (labels * predicted).sum().item()
+        correct += (predicted == labels).sum()
+        # break
+    precision = true_positives*1.0/pred_positives.item()
+    recall = true_positives*1.0/real_positives.item()
+    print(true_positives,pred_positives,real_positives)
+    print("F1 score: {}".format(2*(precision*recall)/(precision+recall)))
+    print('Accuracy of the model: %d %%' % (100 * correct / total))
